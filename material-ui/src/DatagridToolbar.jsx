ac5f32a62957bf0fe37207f4ee3c3d862e09ad6d
Vishal Pal <vishalpal2613@gmail.com>
2025-08-04 20:47:14 +0530

    Adding Search And Row Filters

--- material-ui/src/DatagridToolbar.jsx
+++ material-ui/src/DatagridToolbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { GridToolbarExportContainer } from "@mui/x-data-grid";
+import { GridToolbarExportContainer, GridToolbarFilterButton, GridToolbarQuickFilter } from "@mui/x-data-grid";
 import { Box } from '@mui/system';
 
 const DatagridToolbar = () => {
   return (
-    <Box>
+    <Box display="flex" gap={5}>
         <GridToolbarExportContainer />
+        <GridToolbarFilterButton />
+        <GridToolbarQuickFilter />
     </Box>
   )
 }
